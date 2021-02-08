b5bc0035103cc43be468133c60bb250a44d09cc7
dwkang <ehddn3821@naver.com>
2021-02-08 20:47:30 +0900

    테이블뷰 컨트롤러 추가

---
+++ RealmTodoList/TodoTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TodoTableViewController.swift
+//  RealmTodoList
+//
+//  Created by dwKang on 2021/02/08.
+//
+
+import UIKit
+import RealmSwift
+
+class TodoTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Todo Test"
+        return cell
+    }
+}
